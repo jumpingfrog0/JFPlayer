d0428191db404e19cc8732b2ccbc4882daa0b910
sheldon <dhuang@us517.com>
2016-11-14 01:15:24 +0800

    Initial Commit

---
+++ JFPlayerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JFPlayerDemo
+//
+//  Created by sheldon on 2016/11/14.
+//  Copyright © 2016年 kankan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
